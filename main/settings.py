30febf5ee46669ccb2972452c5cf9d5cceb3353f
osemenjoy <osemenjoy448@gmail.com>
2024-07-07 15:06:16 +0100

    allow render host

--- main/settings.py
+++ main/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG")
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "bookapi-pb21.onrender.com"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "userandorganisationapi-hng-task-2.onrender.com"]
 
 
 # Application definition
